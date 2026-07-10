e917088ac8f2fdaa3a5bd005cf518d894fced3c7
Nipun Goel <118103813+nipungoel24@users.noreply.github.com>
2026-07-10 09:53:22 +0530

    Made global.css for consistent look
    and made the colors more poppy

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
     >
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Outfit:wght@300;400;500;600;700;800&family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" />
       </head>
       <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
